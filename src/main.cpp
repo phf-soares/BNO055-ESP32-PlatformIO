624d09e31f7f0a59668e58ea6b121e27e16d5091
phf-soares <73134614+phf-soares@users.noreply.github.com>
2025-10-24 19:00:16 -0300

    Buffer-ok

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,24 @@
 #include "MySD.h"
 
 const uint8_t INTERRUPT_PIN = 2; // GPIO do ESP32
+const uint8_t CS_PIN = 5;
+#define BUFFER_SIZE 1024
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
 struct BnoState
 {
-  imu::Vector<3> acc;
-  uint32_t start_time, count;
-  float time;
+  imu::Vector<3> acc;  
   float ax, ay, az;
   float axf, ayf, azf;
   float axff, ayff, azff;
+  float time;
   char dataMessage[50];
+  uint32_t start_time, count ;  
 } bnoState;
+
+char buffer[BUFFER_SIZE];
+volatile size_t bufferIndex = 0;
+File dataFile;
 
 void interrupt_callback(void);
 volatile bool interrupt;
@@ -29,8 +35,20 @@ void bno_read(void);
 void setup(void)
 {
   Serial.begin(BAUD_RATE);
+
   bno_setup();
-  sd_setup();
+
+  if (!SD.begin(CS_PIN)) {
+    Serial.println("Falha ao iniciar SD!");
+    while (1);
+  }
+  dataFile = SD.open("/bno.txt", FILE_WRITE);
+  if (!dataFile) {
+    Serial.println("Erro ao abrir arquivo no SD!");
+    while (1);
+  }
+  Serial.println("Inicialização concluída!");
+
   bnoState.start_time = millis();
 }
 
@@ -39,12 +57,35 @@ void loop(void)
   if (interrupt)
   {
     bno_read();
-    SD.begin();  
-    appendFile(SD, "/bno.txt", bnoState.dataMessage);
+    String sample = bnoState.dataMessage;
+
+    // Armazena no buffer
+    size_t len = sample.length();
+    if (bufferIndex + len >= BUFFER_SIZE) {
+      // buffer cheio → grava no SD
+      dataFile.write((uint8_t*)buffer, bufferIndex);
+      dataFile.flush();
+      bufferIndex = 0;  // zera para começar a encher de novo
+    }
+
+    // copia a amostra pro buffer
+    memcpy(&buffer[bufferIndex], sample.c_str(), len);
+    bufferIndex += len;
+
     if( millis() - bnoState.start_time >= 10000) {
-      Serial.print( "Start time: ");    Serial.println( bnoState.start_time); 
-      Serial.print( "End time: ");      Serial.println( millis()); 
-      Serial.print( "Total time (s): ");Serial.println( (millis() - bnoState.start_time) / 1000.0); 
+      dataFile.close();
+      // Ler arquivo
+      Serial.println("\n--- LENDO /bno.txt ---");
+      File readFile = SD.open("/bno.txt", FILE_READ);
+      if (readFile) {
+        while (readFile.available()) Serial.write(readFile.read());
+        readFile.close();
+      }
+      Serial.println("\n--- FIM ---");
+      
+      Serial.print( "Start time: ");    Serial.println( bnoState.start_time/1000.00); 
+      Serial.print( "End time: ");      Serial.println( bnoState.time); 
+      Serial.print( "Total time (s): ");Serial.println( bnoState.time - bnoState.start_time/1000.00); 
       Serial.print( "Total samples: "); Serial.println( bnoState.count);      
       Serial.println("end");
       delay(50000);
