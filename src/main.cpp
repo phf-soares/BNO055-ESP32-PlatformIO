45ad769d39af1a98f5cfa29ea3e8f4bbb2dbd5d7
phf-soares <73134614+phf-soares@users.noreply.github.com>
2025-10-24 02:11:31 -0300

    Solved not monted
    Estava perdendo handle para o SD, verificando com o .begin() está lendo agora

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,100 @@
+#include <Wire.h>
+#include "Adafruit_Sensor.h"
+#include "Adafruit_BNO055.h"
+#include "Arduino.h"
+#include "MyBno_filters.h"
+#include "MyBno_config.h"
 #include "MySD.h"
 
-void setup() {
-  Serial.begin(BAUD_RATE);
-  sd_setup();
+const uint8_t INTERRUPT_PIN = 2; // GPIO do ESP32
 
+Adafruit_BNO055 bno = Adafruit_BNO055();
+struct BnoState
+{
+  imu::Vector<3> acc;
+  float ax, ay, az;
+  float axf, ayf, azf;
+  float axff, ayff, azff;
+} bnoState;
+
+void interrupt_callback(void);
+volatile bool interrupt;
+
+void bno_setup(void);
+void bno_read(void);
+
+void setup(void)
+{
+  Serial.begin(BAUD_RATE);
+  bno_setup();
+  sd_setup();  
 }
 
-void loop() {
-  writeFile(SD, "/bno.txt", "Hello ");
-  appendFile(SD, "/bno.txt", "World!\n");
-  readFile(SD, "/bno.txt"); 
+void loop(void)
+{
+  if (interrupt)
+  {
+    bno_read();
+    if (!SD.begin()) {
+    Serial.println("⚠️ SD não montado, tentando remontar...");
+    if (!SD.begin()) {
+      Serial.println("❌ Falha ao remontar SD!");
+      return;
+    }
+    }
+    writeFile(SD, "/bno.txt", "Hello ");
+    appendFile(SD, "/bno.txt", "World!\n");
+    readFile(SD, "/bno.txt"); 
+  }
+  else
+  {
+    bno.resetInterrupts();
+    interrupt = false;
+  }
+}
+
+void interrupt_callback(void)
+{
+  interrupt = true;
+}
+
+void bno_setup(void)
+{
+  if (!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    ESP.restart();
+  }
+  if (!bno.setAccRange(ACC_RANGE))
+  {
+    Serial.print("Ooops, changing Acc range seems to be refused!");
+  }
+  interrupt = false;
+  pinMode(INTERRUPT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt_callback, RISING);
+  bno.enableAnyMotion(0, 0);
+  bno.enableInterruptsOnXYZ(ENABLE, ENABLE, ENABLE);
+  bno.setExtCrystalUse(true);
+}
+
+void bno_read(void)
+{
+  bnoState.acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  bnoState.ax = bnoState.acc.x();
+  bnoState.ay = bnoState.acc.y();
+  bnoState.az = bnoState.acc.z();
+#if FILTER_ORDER_EMA_FIRST
+  filterEMA(bnoState.ax, bnoState.ay, bnoState.az, EMA_ALPHA,
+            bnoState.axf, bnoState.ayf, bnoState.azf);
+
+  filterMedianN(bnoState.axf, bnoState.ayf, bnoState.azf,
+                bnoState.axff, bnoState.ayff, bnoState.azff);
+#else
+  filterMedianN(bnoState.ax, bnoState.ay, bnoState.az,
+                bnoState.axf, bnoState.ayf, bnoState.azf);
+
+  filterEMA(bnoState.axf, bnoState.ayf, bnoState.azf, EMA_ALPHA,
+            bnoState.axff, bnoState.ayff, bnoState.azff);
+#endif
+  Serial.printf("%.2f,%.2f,%.2f\n", bnoState.axff, bnoState.ayff, bnoState.azff);
 }
